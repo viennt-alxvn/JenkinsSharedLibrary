6f45376d0e68d7a79d5ba41eb4a797ade3cbb16e
viennt-alxvn <55728851+viennt-alxvn@users.noreply.github.com>
2021-01-05 17:37:10 +0700

    up

--- vars/intro.groovy
+++ vars/intro.groovy
@@ -1,7 +1,11 @@
-def call(String name = 'human') {
-  echo "Hello, ${name}."
-}
+// def call(String name = 'human') {
+//   echo "Hello, ${name}."
+// }
 
 def welcome(String name = 'human') {
   echo "Welcome  ${name}!"
+}
+
+def bye(String name = 'human') {
+  echo "See ya  ${name}!"
 }
