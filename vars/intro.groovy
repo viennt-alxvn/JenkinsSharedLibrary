402056b20b35eca5b16348eed9d2c4803922eda6
viennt-alxvn <55728851+viennt-alxvn@users.noreply.github.com>
2021-01-06 14:18:14 +0700

    up

--- vars/intro.groovy
+++ vars/intro.groovy
@@ -5,12 +5,11 @@ def call(String name = 'human') {
 def welcome(String name = 'human') {
   echo "Welcome  ${name}!"
 
-  def per = new utils.DemoClass()
-  per.name = name
-  per.age = 21
-   echo "Info: Name: ${per.name} - Age: ${per.age}"
 }
 
-def bye(String name = 'human') {
-  echo "See ya  ${name}!"
+def info(String name = 'human') {
+    def per = new utils.DemoClass()
+    per.name = name
+    per.age = 21
+    echo "Info: Name: ${per.name} - Age: ${per.age}"
 }
